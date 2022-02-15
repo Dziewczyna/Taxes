ccb4214a84d005c2d81d71fb256423a7a14245a0
Dziewczyna <joanna.podkoscielna@gmail.com>
2022-02-15 18:26:35 +0100

    Updating parametrized tests to more concise

--- src/test/java/org/podatki/PersonTest.java
+++ src/test/java/org/podatki/PersonTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -11,105 +12,82 @@ import org.mockito.MockitoAnnotations;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 class PersonTest {
-    @Mock
-    private TaxesOption taxesOption;
+  public static final BigDecimal SALARY = new BigDecimal("15000");
+  @Mock private TaxesOption taxesOption;
 
-    @BeforeEach
-    public void setUp(){
-        MockitoAnnotations.openMocks(this);
-    }
+  static Stream<Arguments> testMonthlySalary() {
+    return Stream.of(
+        arguments(
+            "Jan",
+            "Kowalski",
+            new BigDecimal("15000.0"),
+            new Married(),
+            new BigDecimal("36000.00")),
+        arguments(
+            "Janina", "Nowak", new BigDecimal("15000.0"), new Single(), new BigDecimal("72000.00")),
+        arguments(
+            "Kasia", "Wójcik", new BigDecimal("15000.0"), new Parent(), new BigDecimal("48000.00")),
+        arguments(
+            "Marcin", "Test", new BigDecimal("1000.0"), new Parent(), new BigDecimal("7000")));
+  }
 
-    @ParameterizedTest
-    @MethodSource("testMonthlySalary")
-    public void shouldPersonWithSalary15000payAnnualTax36000(BigDecimal salary){
-        //given
-        Person married = new Person("Jan", "Kowalski", salary, new Married());
+  static Stream<Arguments> testYearlyIncome() {
+    return Stream.of(arguments("Joanna", "Test", new BigDecimal("10000"), new BigDecimal("5000")));
+  }
 
-        //when
-        var annualTaxMarried = married.payTax();
+  static List<BigDecimal> testSmallSalary() {
+    List<BigDecimal> salaries = new ArrayList<>();
+    salaries.add(new BigDecimal("1000"));
+    return salaries;
+  }
 
-        //then
-        Assertions.assertEquals(annualTaxMarried, new BigDecimal("36000.0"));
-    }
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
 
-    static List<BigDecimal> testMonthlySalary() {
-        List<BigDecimal> salaries = new ArrayList<>();
-        salaries.add(new BigDecimal("15000"));
-        return salaries;
-    }
+  @ParameterizedTest(name = "{index} person {0} {1}({3}) with salary {2} should pay tax {4}")
+  @MethodSource("testMonthlySalary")
+  public void shouldPersonWithSalaryPayCorrespondingAnnualTax(
+      String name, String surname, BigDecimal salary, TaxesOption taxesOption, BigDecimal tax) {
+    // given
+    Person person = new Person(name, surname, salary, taxesOption);
 
-    static List<BigDecimal> testSmallSalary() {
-        List<BigDecimal> salaries = new ArrayList<>();
-        salaries.add(new BigDecimal("1000"));
-        return salaries;
-    }
+    // when
+    var annualTaxMarried = person.payTax();
 
-    @ParameterizedTest
-    @MethodSource("testMonthlySalary")
-    public void shouldSinglePersonWithSalary15000payAnnualTax72000(BigDecimal salary){
+    // then
+    Assertions.assertEquals(tax, annualTaxMarried);
+  }
 
-        //given
-        Person single = new Person("Janina", "Nowak", salary, new Single());
+  @Test
+  public void shouldCorrectlyCalculateYearlyIncome() {
+    // given
+    Person person = new Person("Joanna", "Test", new BigDecimal(10000), new TestTaxesOption());
+    // when
+    var annualTax = person.payTax();
+    // then
+    Assertions.assertEquals(new BigDecimal(10000 * 12), annualTax);
+  }
 
-        //when
-        var annualTaxSingle = single.payTax();
+  @ParameterizedTest(name = "{index} person {0} {1}with salary {2} should pay annual tax {3}")
+  @MethodSource("testYearlyIncome")
+  public void shouldCorrectlyCalculateYearlyIncomeWithMock(
+      String name, String surname, BigDecimal salary, BigDecimal tax) {
 
-        //then
-        Assertions.assertEquals(annualTaxSingle, new BigDecimal("72000.0"));
-    }
-
-    @ParameterizedTest
-    @MethodSource("testMonthlySalary")
-    public void shouldMotherWithSalary15000payAnnualTax48000(BigDecimal salary){
-
-        //given
-        Person mother = new Person("Kasia", "Wójcik", salary, new Parent());
-
-        //when
-        var annualTaxParent = mother.payTax();
-
-        //then
-        Assertions.assertEquals(annualTaxParent, new BigDecimal("48000.0"));
-    }
-
-    @ParameterizedTest
-    @MethodSource("testSmallSalary")
-    public void shouldFatherWithSalary1000payAnnualTax7000(BigDecimal salary){
-        //given
-        Person father = new Person("Marcin", "Test", salary, new Parent());
-
-        //when
-        var annualTaxFather = father.payTax();
-
-        //then
-        Assertions.assertEquals(annualTaxFather, new BigDecimal("7000"));
-    }
-
-    @Test
-    public void shouldCorrectlyCalculateYearlyIncome(){
-        //given
-        Person person = new Person("Joanna","Test",new BigDecimal(10000), new TestTaxesOption());
-        //when
-        var annualTax = person.payTax();
-        //then
-        Assertions.assertEquals(new BigDecimal(10000 * 12),annualTax);
-    }
-
-    @Test
-    public void shouldCorrectlyCalculateYearlyIncomeWithMock(){
-
-        //given
-        when(taxesOption.payTaxes(eq(new BigDecimal(10000*12)))).thenReturn(new BigDecimal(5000));
-        Person person = new Person("Joanna","Test",new BigDecimal(10000), taxesOption);
-        //when
-        var annualTax = person.payTax();
-        //then
-        Assertions.assertEquals(new BigDecimal(5000),annualTax);
-
-    }
+    // given
+    when(taxesOption.payTaxes(eq(new BigDecimal(10000 * 12)))).thenReturn(tax);
+    Person person = new Person(name, surname, salary, taxesOption);
+    // when
+    var annualTax = person.payTax();
+    // then
+    Assertions.assertEquals(tax, annualTax);
+  }
 }
